30a7b436c5173fa5a92c7deb5df6ae235c20ad04
Lukas Herman <lukash@g.clemson.edu>
2018-09-03 20:53:44 -0400

    Refractored

--- assignment-1/main.cu
+++ assignment-1/main.cu
@@ -4,6 +4,30 @@
 float *CPU_big_dot(float *A, float *B, int N);
 float *GPU_big_dot(float *A, float *B, int N);
 
+/* Helper functions */
+float *range(float start, float end, float step);
+void print_vector(float *ar, int N);
+
+int main(){
+  int n = 10;
+  float *a = range(1.0, 1.0 + n, 1.0);
+  float *b = range(2.0, 2.0 + n, 1.0);
+  float *results = CPU_big_dot(a, b, n);  
+
+  print_vector(results, n);
+
+  free(results);
+  free(a);
+  free(b);
+}
+
+float *CPU_big_dot(float *A, float *B, int N){
+  float *results = (float *)malloc(sizeof(float) * N);
+  for(int i = 0; i < N; i++)
+    results[i] = A[i] * B[i];
+  return results;
+}
+
 float *range(float start, float end, float step){
   int n = (int)(end - start) / step + 1;
   float *results = (float *)malloc(sizeof(float) * n);
@@ -13,17 +37,10 @@ float *range(float start, float end, float step){
     results[i] = current; 
     current += step;
   }
-
   return results;
 }
 
-int main(){
-  float *a = range(1.0, 10.0, 1.0);
-  float *b = range(2.0, 11.0, 1.0);
-  float *results = CPU_big_dot(a, b, 10);  
-
-  printf("%f", 
-
-  free(a);
-  free(b);
+void print_vector(float *ar, int N){
+  for(int i = 0; i < N; i++)
+    printf("%f\n", ar[i]);
 }
